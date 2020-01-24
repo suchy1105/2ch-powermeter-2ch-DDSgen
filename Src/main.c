063fb349650fa2e023fdbf2d044a802a954c5376
suchy1105 <60182725+suchy1105@users.noreply.github.com>
2020-01-24 07:21:48 +0100

    Add files via upload

---
+++ Src/main.c
@@ -0,0 +1,502 @@
+
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2019 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+uint32_t PomiarADC;
+uint32_t frik;
+uint32_t aktualny;
+uint32_t resultF;
+uint32_t rfreq;
+uint32_t VoPM;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_ADC_Start(&hadc1);
+
+  //Begin of Serial initialization sequence
+
+  HAL_GPIO_WritePin(CLK_GPIO_Port,CLK_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(CLK_GPIO_Port,CLK_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(CLK_GPIO_Port,CLK_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(FQ_UP_GPIO_Port,FQ_UP_Pin,GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(FQ_UP_GPIO_Port,FQ_UP_Pin,GPIO_PIN_SET);
+  HAL_GPIO_WritePin(FQ_UP_GPIO_Port,FQ_UP_Pin,GPIO_PIN_RESET);
+
+ firstiterationLV();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+	  if(rfreq<(26000000) || rfreq>(28000000))
+	 	  {
+	 		   firstiterationLV();
+	 	  }
+
+	 	  modulationLV2();
+
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* ADC1 init function */
+static void MX_ADC1_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_12;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, FQ_UP_Pin|RST_Pin|DATA_Pin|CLK_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : FQ_UP_Pin RST_Pin DATA_Pin CLK_Pin */
+  GPIO_InitStruct.Pin = FQ_UP_Pin|RST_Pin|DATA_Pin|CLK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+void outOne() {
+	HAL_GPIO_WritePin(CLK_GPIO_Port,CLK_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DATA_GPIO_Port, DATA_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(CLK_GPIO_Port,CLK_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(DATA_GPIO_Port, DATA_Pin, GPIO_PIN_RESET);
+
+}
+void outZero() {
+	HAL_GPIO_WritePin(CLK_GPIO_Port,CLK_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DATA_GPIO_Port, DATA_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(CLK_GPIO_Port,CLK_Pin, GPIO_PIN_SET);
+}
+void lastWord()
+{
+	//W0 = 00001001	last word		Final control byte, all 0x09 for 9851 chip
+		outOne();
+		outZero();
+		outZero();
+		outOne();
+		outZero();
+		outZero();
+		outZero();
+		outZero();
+}
+
+void sendFrequency(uint32_t frequency) {
+
+
+	uint32_t freq =(frequency * 4294967295)/(150000000);
+
+for(uint32_t i=0;i<32;i++)
+		  {if (freq%2==1)
+			  outOne();
+		  else
+			  outZero();
+		        freq = freq >> 1;
+		  }
+lastWord();
+
+   HAL_GPIO_WritePin(FQ_UP_GPIO_Port,FQ_UP_Pin,GPIO_PIN_SET);
+   HAL_GPIO_WritePin(FQ_UP_GPIO_Port,FQ_UP_Pin,GPIO_PIN_RESET);
+}
+void MeasureADC(uint32_t iterations)
+{
+	for (uint32_t i=0; i<iterations ; i++)
+	{
+		if(HAL_ADC_PollForConversion(&hadc1, 10)==HAL_OK)
+			{
+				PomiarADC=PomiarADC+HAL_ADC_GetValue(&hadc1);
+				HAL_ADC_Start(&hadc1);
+			}
+	} PomiarADC=PomiarADC/iterations;
+
+}
+
+//first iteration from 26,5MHz to 27,5MHz 	 accuracy 10kHz
+
+void firstiterationLV()
+{
+	int beginF=25000000;
+	int endF=29000000;
+	int diffF=endF-beginF;
+	int stepF=diffF/100;
+	//int stepF=;
+	int firstF=0, secondF=0;
+	int lowest1V=4096,lowest2V=4097;
+	int frequency=0;
+	for(int i=0;i<100;i++)
+	{
+		frequency =stepF*i+beginF;//frequency =i*1000000;
+		sendFrequency(frequency);
+		MeasureADC(150);
+
+	   if(lowest1V>PomiarADC)
+	   {
+		   secondF=firstF;
+		   lowest2V=lowest1V;
+		   firstF=frequency;
+		   lowest1V=PomiarADC;
+	   }
+	   else if(lowest2V>PomiarADC)
+	   {
+		   secondF=frequency;
+		   lowest2V=PomiarADC;
+	   }
+	}
+	/***************************************************************
+	 *
+	 *
+	 * 		Second iteration
+	 * 		accuracy 10kHz
+	 *
+	 * *************************************************************/
+		if(firstF<secondF)
+		{
+			beginF=firstF;
+			endF=secondF;
+		}
+		else
+		{
+			endF=firstF;
+			beginF=secondF;
+		}
+		diffF=endF-beginF;
+		stepF=diffF/10;
+		for(int j=0;j<10;j++)
+			{
+				frequency =stepF*j+beginF;
+				sendFrequency(frequency);
+				MeasureADC(15);
+
+			   if(lowest1V>PomiarADC)
+			   {
+				   secondF=firstF;
+				   lowest2V=lowest1V;
+				   firstF=frequency;
+				   lowest1V=PomiarADC;
+			   }
+			   else if(lowest2V>PomiarADC)
+			   {
+				   secondF=frequency;
+				   lowest2V=PomiarADC;
+			   }
+			}
+
+	/***************************************************************
+	 *
+	 *
+	 * 		Third iteration
+	 *		accuracy 100Hz
+	 *
+	 * *************************************************************/
+		if(firstF<secondF)
+		{
+			beginF=firstF;
+			endF=secondF;
+		}
+		else
+		{
+			endF=firstF;
+			beginF=secondF;
+		}
+		diffF=endF-beginF;
+		stepF=diffF/10;
+		for(int j=0;j<10;j++)
+			{
+				frequency =stepF*j+beginF;
+				sendFrequency(frequency);
+				MeasureADC(15);
+
+			   if(lowest1V>PomiarADC)
+			   {
+				   secondF=firstF;
+				   lowest2V=lowest1V;
+				   firstF=frequency;
+				   lowest1V=PomiarADC;
+			   }
+			   else if(lowest2V>PomiarADC)
+			   {
+				   secondF=frequency;
+				   lowest2V=PomiarADC;
+			   }
+			}
+
+	sendFrequency(firstF);
+	rfreq=firstF;
+}
+
+void modulationLV2()//Perturbation and observe method (P&O)
+{
+	uint32_t FStep=5000;//Hz
+	MeasureADC(800);
+	int test1F=rfreq-FStep;
+	int test2F=rfreq+FStep;
+	/*present freq*/
+	VoPM=PomiarADC;
+
+	/*test lower*/
+	sendFrequency(test1F);
+	MeasureADC(800);
+	int lowerTestF_ADC=PomiarADC;
+	/*test higher*/
+	sendFrequency(test1F);
+	MeasureADC(800);
+	int higherTestF_ADC=PomiarADC;
+
+	if(lowerTestF_ADC<VoPM)
+	{
+		rfreq=test1F;
+	}
+	else if(higherTestF_ADC<VoPM)
+	{
+		rfreq=test2F;
+	}
+	sendFrequency(rfreq);
+
+}
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
